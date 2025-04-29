7effdfafdf62299d0aedb2a19d000a5c32e644dc
yzemlyak <yzemlyak@gmail.com>
2025-04-29 01:40:40 +0000

    added more routes

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,17 +1,30 @@
 class ZebraController < ApplicationController
   def homepage
+    @the_num = params.fetch("user_number").to_f
     render({:template => "game_templates/homepage"})
   end
   def square_new
     render({:template => "game_templates/square_new"})
   end
+  def square_new_results
+    render({:template => "game_templates/square_new_results"})
+  end
   def square_root_new
     render({:template => "game_templates/square_root_new"})
+  end
+  def square_root_new_results
+    render({:template => "game_templates/square_root_new_results"})
   end
   def payment_new
     render({:template => "game_templates/payment_new"})
   end
+  def payment_new_results
+    render({:template => "game_templates/payment_new_results"})
+  end
   def random_new
     render({:template => "game_templates/random_new"})
+  end
+  def random_new_results
+    render({:template => "game_templates/random_new_results"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "zebra",:action => "homepage" })
   get("/square/new", { :controller => "zebra",:action => "square_new" })
+  get("/square/new/results", { :controller => "zebra",:action => "square_new_results" })
   get("/square_root/new", { :controller => "zebra",:action => "square_root_new" })
+  get("/square_root/new/results", { :controller => "zebra",:action => "square_root_new_results" })
   get("/payment/new", { :controller => "zebra",:action => "payment_new" })
+  get("/payment/new/results", { :controller => "zebra",:action => "payment_new_results" })
   get("/random/new", { :controller => "zebra",:action => "random_new" })
+  get("/random/new/results", { :controller => "zebra",:action => "random_new_results" })
 end
